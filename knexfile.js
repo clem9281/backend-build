175db10cacd1f03ac948157db876b4878f5dfec8
clem9281 <clem9281@gmail.com>
2019-07-24 14:17:59 -0700

    added packages, configured knexfile

---
+++ knexfile.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/backend_build.sqlite3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done); // enforce FK
+      }
+    }
+  },
+
+  testing: {
+    client: "pg",
+    connection: {
+      host: "127.0.0.1",
+      user: "postgres",
+      password: process.env.LOCAL_PG_PASS,
+      database: "backend_build_test"
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  },
+
+  production: {
+    client: "postgresql",
+    connection: process.env.DB_CONNECTION || {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  }
+};
