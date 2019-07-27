ef2607405601164abdaafd8b72124b0726d425ce
clem9281 <clem9281@gmail.com>
2019-07-27 16:24:45 -0700

    added more variation and completion for habits seeds

--- data/seeds/005-userCompleted.js
+++ data/seeds/005-userCompleted.js
@@ -3,6 +3,34 @@ const moment = require("moment");
 exports.seed = function(knex) {
   return knex("userCompleted").insert([
     { userHabit_id: 1, completed_date: moment().format("YYYY-M-D") },
-    { userHabit_id: 3, completed_date: moment().format("YYYY-M-D") }
+    {
+      userHabit_id: 1,
+      completed_date: moment("2019-06-30").format("YYYY-M-D")
+    },
+    {
+      userHabit_id: 1,
+      completed_date: moment("2019-07-05").format("YYYY-M-D")
+    },
+    {
+      userHabit_id: 1,
+      completed_date: moment("2019-07-10").format("YYYY-M-D")
+    },
+    {
+      userHabit_id: 1,
+      completed_date: moment("2019-07-11").format("YYYY-M-D")
+    },
+    {
+      userHabit_id: 1,
+      completed_date: moment("2019-07-18").format("YYYY-M-D")
+    },
+    {
+      userHabit_id: 1,
+      completed_date: moment("2019-07-20").format("YYYY-M-D")
+    },
+    {
+      userHabit_id: 1,
+      completed_date: moment("2019-07-21").format("YYYY-M-D")
+    },
+    { userHabit_id: 1, completed_date: moment("2019-23-07").format("YYYY-M-D") }
   ]);
 };
